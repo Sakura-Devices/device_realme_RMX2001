bfd8aaf76e538e89c2266ed729e026ed3a7e1cff
Atlas78 <76161855+Atlas78@users.noreply.github.com>
2021-05-18 20:49:47 +0530

    RMX2001: Set priv app log level to log
     * microg packages needs it

--- system_prop.mk
+++ system_prop.mk
@@ -72,3 +72,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.dha_th_rate=2.3 \
     ro.config.sdha_apps_bg_max=64 \
     ro.config.sdha_apps_bg_min=8
+
+# Priv-app permission
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.control_privapp_permissions=log
