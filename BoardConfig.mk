7e8af94da96a5a06fad44d9cb73cd6f1f2974282
sharanpai3 <sharanpai3@gmail.com>
2021-07-07 13:40:05 +0530

    RMX2001: Panic to recovery, not BL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,7 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 # BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x40078000
