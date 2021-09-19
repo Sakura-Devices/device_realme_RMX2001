2afe4eb04748ccc023f3978a3f830f69bbb8c825
sharanpai3 <sharanpai3@gmail.com>
2021-09-19 20:44:25 +0530

    RMX2001: Welcome Sakura!

--- lineage_RMX2001.mk
+++ lineage_RMX2001.mk
@@ -21,26 +21,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2001/device.mk)
 
-# Inherit some common Octavi stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Octavi Properties
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SCREEN_DENSITY := 420
-TARGET_USES_BLUR := true
+# Sakura build flags
+SAKURA_OFFICIAL := true
+SAKURA_MAINTAINER := Sharan
+SAKURA_BUILD_TYPE=coregapps
 TARGET_FACE_UNLOCK_SUPPORTED := true
-WITH_GAPPS := true
-OCTAVI_BUILD_TYPE := Official
-OCTAVI_DEVICE_MAINTAINER := sharanpai
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := octavi_RMX2001
+PRODUCT_NAME := lineage_RMX2001
 PRODUCT_DEVICE := RMX2001
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G90T Series
 PRODUCT_MANUFACTURER := realme
 
 # Build info
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2001 \
+    PRODUCT_NAME=RMX2001 \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
